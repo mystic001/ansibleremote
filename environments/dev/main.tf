e9dd1309e117e7b8e5a4e3fdcec23696bc307a7f
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2025-05-23 05:16:11 -0500

    Enable Azure AD authentication in Terraform configurations and uncomment VNet module setup in dev and prod environments

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name = "tfstate14839"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
+    use_azuread_auth     = true
   }
 }
 
@@ -13,27 +14,27 @@ resource "random_string" "suffix" {
   upper   = false
 }
 
-# module "vnet" {
-#   source = "../../"
+module "vnet" {
+  source = "../../"
 
-#   resource_group_name = "dev-vnet-rg"
-#   location           = "eastus"
-#   vnet_name          = "dev-vnet"
-#   address_space      = ["10.1.0.0/16"]
+  resource_group_name = "dev-vnet-rg"
+  location           = "eastus"
+  vnet_name          = "dev-vnet"
+  address_space      = ["10.1.0.0/16"]
 
-#   subnets = {
-#     subnet1 = {
-#       name             = "dev-subnet1"
-#       address_prefixes = ["10.1.1.0/24"]
-#     }
-#     subnet2 = {
-#       name             = "dev-subnet2"
-#       address_prefixes = ["10.1.2.0/24"]
-#     }
-#   }
+  subnets = {
+    subnet1 = {
+      name             = "dev-subnet1"
+      address_prefixes = ["10.1.1.0/24"]
+    }
+    subnet2 = {
+      name             = "dev-subnet2"
+      address_prefixes = ["10.1.2.0/24"]
+    }
+  }
 
-#   tags = {
-#     Environment = "Development"
-#     Project     = "MyProject"
-#   }
-# } 
+  tags = {
+    Environment = "Development"
+    Project     = "MyProject"
+  }
+} 
